1c83b367b50405e8ec1ba2f12b36de1d7675591a
Siedovolosij Serhij <sedovolosiy@gmail.com>
2016-04-11 18:11:28 +0300

    Статус работает, визуализация во вьюхе skills подключена.

--- yii-application/backend/views/skills/index.php
+++ yii-application/backend/views/skills/index.php
@@ -1,13 +1,12 @@
 <?php
-
 use yii\helpers\Html;
 use yii\grid\GridView;
 use yii\widgets\Pjax;
+use yii\bootstrap\Progress;
 //use Yii;
 /* @var $this yii\web\View */
 /* @var $searchModel backend\models\SkillsSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
-
 $this->title = 'Навыки';
 $this->params['breadcrumbs'][] = $this->title;
 $this->registerJsFile('/js/visible.js');
@@ -20,56 +19,70 @@ $this->registerJsFile('/js/visible.js');
     <p>
         <?= Html::a('Добавить навыки', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
-<?php Pjax::begin(); ?>    <?= GridView::widget([
+    <?php Pjax::begin(); ?>    <?= GridView::widget([
         'dataProvider' => $dataProvider,
 //        'filterModel' => $searchModel,
         'columns' => [
             ['class' => 'yii\grid\SerialColumn'],
-
-            'id',
             'title',
-            'value',
-//            'status:boolean',
             [
+                'attribute' => 'value',
+                'header' => 'Значение',
+                'content' => function ($model) {
+                    if ($model->value <= 50):
+                        return Progress::widget([
+                            'percent' => $model->value,
+                            'label' => $model->value . ' %',
+                            'barOptions' => ['class' => 'progress-bar-danger']
+                        ]);
+                    elseif ($model->value <= 70):
+                        return Progress::widget([
+                            'percent' => $model->value,
+                            'label' => $model->value . ' %',
+                            'barOptions' => ['class' => 'progress-bar-warning']
+                        ]);
+                    elseif ($model->value <= 100):
+                        return Progress::widget([
+                            'percent' => $model->value,
+                            'label' => $model->value . ' %',
+                            'barOptions' => ['class' => 'progress-bar-success']
+                        ]);
+                    endif;
+                }
+            ],
+            [
+                'header' => 'Статус',
 //                'attribute'=>'status',
-                'header'=>'Статус',
                 'contentOptions' =>['style'=>'display:block;'],
                 'headerOptions' => ['width' => '80'],
-                'content'=>function($model){
+                'content' => function ($model) {
                     return $model->status == 1 ? Html::button('<i class="fa fa-eye"></i> Active',
-                    [
-                        'title' => 'Status',
-                        'class' => 'status-button',
-                        'data-href'=> '/skills/change-visible',
-                        'data-status'=> $model->status,
-                        'data-id' => $model->id,
-                        'data-method' => 'post',
-
-                    ])
-                    :
+                        [
+                            'title' => 'Status',
+                            'class' => 'status-button btn btn-primary btn-xs btn-success',
+                            'data-href' => '/skills/change-visible',
+                            'data-status' => $model->status,
+                            'data-id' => $model->id,
+                            'data-method' => 'post',
+                        ])
+                        :
                         Html::button('<i class="fa fa-eye-slash"></i> Pass',
                             [
                                 'title' => 'Status',
-                                'class' => 'status-button',
-                                'data-href'=> '/skills/change-visible',
-                                'data-status'=> $model->status,
+                                'class' => 'status-button btn btn-primary btn-xs btn-warning',
+                                'data-href' => '/skills/change-visible',
+                                'data-status' => $model->status,
                                 'data-id' => $model->id,
                                 'data-method' => 'post',
-
-
                             ]);
                 }
             ],
-
             [
                 'class' => 'yii\grid\ActionColumn',
-                'header'=>'Действия',
+                'header' => 'Действия',
                 'headerOptions' => ['width' => '80'],
                 'template' => '{view} {update} {delete}',
-
-
             ],
-
         ]
     ]); ?>
-<?php Pjax::end(); ?></div>
+    <?php Pjax::end(); ?></div>
