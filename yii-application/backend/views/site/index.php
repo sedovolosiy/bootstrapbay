249686c5c4476db288234c19d7a486ae9c73f92b
Siedovolosij Serhij <sedovolosiy@gmail.com>
2016-04-11 20:47:33 +0300

    Вывод роли на главной, статус работает

--- yii-application/backend/views/site/index.php
+++ yii-application/backend/views/site/index.php
@@ -1,7 +1,6 @@
 <?php
-
+use yii\helpers\ArrayHelper;
 /* @var $this yii\web\View */
-
 $this->title = 'My Yii Application';
 ?>
 <div class="site-index">
@@ -10,12 +9,24 @@ $this->title = 'My Yii Application';
         <h1>Админка</h1>
 
         <p class="lead">Вы вошли в административную часть сайта. Выберите нужный пункт меню.</p>
-
+        <?php if (Yii::$app->user->can('admin')) {
+            $userRole = Yii::$app->authManager->getRolesByUser(Yii::$app->user->id);
+            $value = ArrayHelper::getValue($userRole, 'admin.description');
+            echo "Роль присвоена $value";
+        } elseif (Yii::$app->user->can('editor')) {
+            $userRole = Yii::$app->authManager->getRolesByUser(Yii::$app->user->id);
+            $value = ArrayHelper::getValue($userRole, 'editor.description');
+            echo "Роль присвоена $value";
+        } else {
+            echo 'Роль присвоена Пользователь';
+        }
+        ?>
     </div>
 
     <div class="body-content">
 
         <div class="row">
+
 
         </div>
 
